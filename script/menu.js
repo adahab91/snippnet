aae5ff000a611151783d39150e310698b107855b
adahab91 <adahaberg@Adas-MacBook-Pro.local>
2019-06-18 15:25:23 +0200

    18.06.19

--- script/menu.js
+++ script/menu.js
@@ -1,16 +1,57 @@
+window.addEventListener("scroll",function(){
+  var snippnetLogo = document.getElementById("snippnetLogo");
+  var snippnetLogoSm = this.document.getElementById("snippnetLogoSm");
+  if(window.pageYOffset > 200){
+   snippnetLogo.style.top = "-200px";
+   snippnetLogo.style.transition = "all 0.3s linear";
+  } else{
+      snippnetLogo.style.top = "20px";
+  }
+  if(window.pageYOffset < 200){
+    snippnetLogoSm.style.top = "-200px";
+    snippnetLogoSm.style.transition = "all 0.3s linear";
+  } else{
+      snippnetLogoSm.style.top = "20px";
+  }
+},false);
+
+
+
+
+
+
+
+/* //HAMBURGER MENU
 let toggleIcon = document.getElementById("toggleIcon");
 let menuLinks = document.getElementById("menuLinks");
 
 toggleIcon.addEventListener("click", function(){
     if (menuLinks.dataset.toggle === "off"){
         menuLinks.dataset.toggle = "on";
+        document.getElementById("hamburger").style.backgroundColor = "white";
+
     } else {
         menuLinks.dataset.toggle = "off";
     }
 
-    if(toggleIcon.dataset.icon === "on"){
+     if(toggleIcon.dataset.icon === "on"){
         toggleIcon.dataset.icon = "off";
     } else {
         toggleIcon.dataset.icon = "on";
-    }
+    } 
+
 });
+
+if (toggleIcon.dataset.icon === "on"){
+    menuLinks.dataset.toggle = "off";
+} else if (menuLinks.dataset.toggle === "on"){
+    toggleIcon.dataset.toggle = "off";
+}
+
+function collapseMenu(){
+    toggleIcon.dataset.icon = "on";
+    menuLinks.dataset.toggle = "off";
+}
+
+window.addEventListener("scroll", collapseMenu);
+ */
