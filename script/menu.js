a6580f9f9e9243124815cb8b01060eb425db85c5
adahab91 <adahaberg@Adas-MacBook-Pro.local>
2019-06-24 08:46:39 +0200

    fixed logo

--- script/menu.js
+++ script/menu.js
@@ -1,4 +1,4 @@
-window.addEventListener("scroll",function(){
+/* window.addEventListener("scroll",function(){
   var snippnetLogo = document.getElementById("snippnetLogo");
   var snippnetLogoSm = this.document.getElementById("snippnetLogoSm");
   if(window.pageYOffset > 200){
@@ -7,15 +7,52 @@ window.addEventListener("scroll",function(){
   } else{
       snippnetLogo.style.top = "20px";
   }
-  if(window.pageYOffset < 200){
+  if(window.pageYOffset < 201){
     snippnetLogoSm.style.top = "-200px";
     snippnetLogoSm.style.transition = "all 0.3s linear";
   } else{
       snippnetLogoSm.style.top = "20px";
   }
+},false); */  //gammal kode som eg ikkje vil øydelegge
+  var oldValue = 0;
+window.addEventListener("scroll",function(e){
+  var snippnetLogo = document.getElementById("snippnetLogo");
+  var snippnetLogoDown = this.document.getElementById("snippnetLogoDown");
+  var snippnetLogoUp = this.document.getElementById("snippnetLogoUp");
+  
+  //var st = window.pageYOffset || this.document.documentElement.scrollTop;
+
+  var newValue = window.pageYOffset;
+  
+  if(window.pageYOffset > 200){
+   snippnetLogo.style.opacity= "0";
+    snippnetLogo.style.transition = "all 0.5s";
+  } else{
+      snippnetLogo.style.opacity = "1";
+  }
+  if(window.pageYOffset < 201){
+    //snippnetLogoDown.style.top = "-200px";
+    snippnetLogoDown.style.opacity = "0";
+    snippnetLogoDown.style.transition = "all 0.3s";
+
+    snippnetLogoUp.style.opacity = "0";
+    snippnetLogoUp.style.transition = "all 0.3s";
+  } else{
+        console.log(newValue + "new value");
+        console.log(oldValue + "old value");
+       if (oldValue - newValue < 0){
+            snippnetLogoDown.style.opacity = "1";
+            snippnetLogoUp.style.opacity = "0";
+            console.log("down");
+        } else if (oldValue - newValue > 0){
+            snippnetLogoDown.style.opacity = "0";
+            snippnetLogoUp.style.opacity = "1";
+            console.log("up");
+        }
+        oldValue = newValue;
+  }
+
 },false);
-
-
 
 
 
