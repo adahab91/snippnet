d10b6b044277fb4db7b1b367965a7ee32c6e06c4
adahab91 <adahaberg@Adas-MacBook-Pro.local>
2019-10-29 15:09:47 +0100

    Started updating my homepage

--- script/scroll-top.js
+++ script/scroll-top.js
@@ -1,8 +1,10 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+window.addEventListener("scroll",function(e){
+  var myArrow = document.getElementById("arrow-down");
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  if(window.pageYOffset > 20){
+   myArrow.style.opacity= "0";
+    myArrow.style.transition = "all 0.2s";
+  } else{
+      myArrow.style.opacity = "1";
+  }
+},false);
